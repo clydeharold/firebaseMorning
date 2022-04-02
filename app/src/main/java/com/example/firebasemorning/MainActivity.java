aa77af28ba91bf73da31722a89bdf9a7c5602581
clydeharold <ch.paronda@gmail.com>
2022-04-02 11:59:34 +0800

    Added retrive function

--- app/src/main/java/com/example/firebasemorning/MainActivity.java
+++ app/src/main/java/com/example/firebasemorning/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.firebasemorning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button cmdAdd;
+    Button cmdAdd, cmdSearch;
     EditText etId, etName, etCourse;
+    ArrayList<Student> students = new ArrayList<>();
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Students");
@@ -25,14 +34,58 @@ public class MainActivity extends AppCompatActivity {
         refs();
 
         cmdAdd.setOnClickListener(addStudentRecord);
+        cmdSearch.setOnClickListener(searchStudentId);
+        addValuEventListener();
+
     }
 
+
+    public void addValuEventListener() {
+        myRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    for(DataSnapshot ds: task.getResult().getChildren()){
+                        students.add(ds.getValue(Student.class));
+                    }
+                }
+            }
+        });
+    }
     public void refs() {
         cmdAdd = findViewById(R.id.cmdAdd);
+        cmdSearch = findViewById(R.id.cmdSearch);
         etId = findViewById(R.id.etStudentId);
         etName = findViewById(R.id.etStudentName);
         etCourse = findViewById(R.id.etStudentCourse);
     }
+
+    View.OnClickListener searchStudentId = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Toast.makeText(getApplicationContext(),"Size: " + students.size(),Toast.LENGTH_SHORT).show();
+
+            int id = Integer.parseInt(etId.getText().toString()), flag = 0;
+            for(Student s: students){
+                if(id == s.getId()) {
+                    etName.setText(s.getName());
+                    etCourse.setText(s.getCourse());
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if(flag == 0) {
+                Toast.makeText(getApplicationContext(),"Record not Found.",Toast.LENGTH_SHORT).show();
+                etName.setText("");
+                etCourse.setText("");
+            }
+
+        }
+    };
 
     View.OnClickListener addStudentRecord = new View.OnClickListener() {
         @Override
@@ -46,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
             Student stud = new Student(id, name, course);
             myRef.child(etId.getText().toString()).setValue(stud);
+            addValuEventListener();
         }
     };
 
